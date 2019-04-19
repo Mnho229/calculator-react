969a65b1ab69242a40df9f05226cdfc4ec8db81e
Mnho229 <michaelho3624@gmail.com>
2019-04-19 15:12:37 -0400

    new project

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../dist/css/main.min.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="test">test</div>
+    )
+  }
+}
+
+export default Main;
